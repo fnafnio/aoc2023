1aa4bf088b19999b732981d8bb314c8ecbee2224
Fabio Pungg <fabio.pungg@medel.com>
2023-12-04 15:08:50 +0100

    the regex crate is weird, I still don't get it

--- src/solutions/day_3.rs
+++ src/solutions/day_3.rs
@@ -1,3 +1,7 @@
+use std::{fmt::Display, ops::Range};
+
+use regex::{Match, Regex};
+
 use crate::Solver;
 
 pub struct Day;
@@ -9,6 +13,42 @@ impl Solver for Day {
 
     fn part_2(&self, input: &str) -> String {
         todo!()
+    }
+}
+
+#[derive(Debug)]
+struct Schematic<'a> {
+    data: Vec<&'a str>,
+}
+
+impl<'a> Display for Schematic<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for &l in self.data.iter() {
+            f.write_fmt(format_args!("{l}\n"));
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Schematic<'a> {
+    fn create(input: &'a str) -> Self {
+        Self {
+            data: input.lines().filter(|l| !l.is_empty()).collect(),
+        }
+    }
+
+    fn get_numbers(&self) -> Vec<(usize, Match)> {
+        let mut v = vec![];
+        let re = Regex::new(r#"\D*([0-9]+)\D*"#).unwrap();
+
+        for (i, &l) in self.data.iter().enumerate() {
+            if let Some(c)= re.captures(l) {
+            for m in c.get(1) {
+                let range = m.range();
+                v.push((i, m));
+            }
+        }}
+        dbg!(v)
     }
 }
 
@@ -26,8 +66,10 @@ mod tests {
 ...$.*....
 .664.598.."#;
 
-#[test]
-fn part_1() {
-    
-}
+    #[test]
+    fn part_1() {
+        let schem = Schematic::create(INPUT);
+        schem.get_numbers();
+        println!("{schem}");
+    }
 }
